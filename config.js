12965b5396cb3f10f71e94657fbbc8edaf027849
Sufyan Ashraf <sufyanashraf312@gmail.com>
2021-12-24 04:20:51 +0500

    second commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-const server = dev ? 'http://localhost:3000' : 'https://next-js-app-ten.vercel.app';
+const server = dev ? 'http://localhost:3000' : 'https://shopify-invoice-app.vercel.app';
 const password = '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';
 const key = 'b9a5814dc31133c0c9214b7e4f1b0b77';
 const shop = 'royalgalleriapk';
