4b0152048ede902e67b72d2093a42efcc4ed880c
sufyan-ashraf <30364936+sufyan-ashraf@users.noreply.github.com>
2023-09-14 21:20:18 +0500

    Update config.js and rmoving dummy keys

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const dev = process.env.NODE_ENV !== 'production';
 
 const server = dev ? 'http://localhost:3000' : 'https://shopify-invoice-app.vercel.app';
-const password = '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';
-const key = 'b9a5814dc31133c0c9214b7e4f1b0b77';
-const shop = 'royalgalleriapk';
+const password = process.env.YOUR_SHOPIFY_APP_PASSWORD;
+const key = process.env.YOUR_SHOPIFY_APP_KEY;
+const shop = process.env.YOUR_SHOPIFY_APP_NAME;
 
 export {server, password, key, shop};
